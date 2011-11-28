617c30db587cd7ba650e6cb1dce62b8ac6111da8
Joe Stump <joe@joestump.net>
2011-11-28 11:20:40 -0800

    Added a basic setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-airbrake',
+    version='0.0.1',
+    description='A Django app for submitting exceptions to Airbrake.io.',
+    long_description='',
+    keywords='django, airbrake',
+    author='Joseph C. Stump',
+    author_email='joe@stu.mp',
+    url='https://github.com/joestump/django-airbrake',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['decorator', 'djutils'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ]
+)
