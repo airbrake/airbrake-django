2422e76454585538bcb8af8e1b085b9abfe776b8
Nicolas Marlier <nicolas@juliedesk.com>
2017-02-13 12:06:58 +0100

    Automatic error sending with debugged middleware

--- airbrake/middleware.py
+++ airbrake/middleware.py
@@ -1,13 +1,10 @@
 from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
-from django.utils.deprecation import MiddlewareMixin
 from airbrake.utils.client import Client
 
-class AirbrakeNotifierMiddleware(MiddlewareMixin):
+class AirbrakeNotifierMiddleware(object):
     def __init__(self, *args, **kwargs):
         self.client = Client()
-
-        super(AirbrakeNotifierMiddleware, self).__init__(*args, **kwargs)
 
     def process_exception(self, request, exception):
         if hasattr(settings, 'AIRBRAKE') and not settings.AIRBRAKE.get('DISABLE', False):
