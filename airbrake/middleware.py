15010b4700cc7c5b7bec46ed21c4dd21207a30bf
Nicolas Marlier <nicolas@juliedesk.com>
2017-02-15 13:26:34 +0100

    Support for bor pre-django 1.10 and post in middleware

--- airbrake/middleware.py
+++ airbrake/middleware.py
@@ -1,10 +1,17 @@
 from django.conf import settings
-from django.core.exceptions import MiddlewareNotUsed
+try:
+    # MiddlewareMixin is not available anymore in Django 1.8.7
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
 from airbrake.utils.client import Client
 
-class AirbrakeNotifierMiddleware(object):
+
+class AirbrakeNotifierMiddleware(MiddlewareMixin):
     def __init__(self, *args, **kwargs):
         self.client = Client()
+
+        super(AirbrakeNotifierMiddleware, self).__init__(*args, **kwargs)
 
     def process_exception(self, request, exception):
         if hasattr(settings, 'AIRBRAKE') and not settings.AIRBRAKE.get('DISABLE', False):
