27434f84fad114c77970971df223d56af09aba27
Ben Petty <benjamin.s.petty@gmail.com>
2018-06-12 09:59:43 -0700

    Update decorators.py
    Fixes #22

--- airbrake/decorators.py
+++ airbrake/decorators.py
@@ -2,7 +2,7 @@ def airbrake(f):
     def __inner__(*args, **kwargs):
         try:
             f(*args, **kwargs)
-        except Exception, e:
+        except Exception as e:
             from airbrake.utils.client import Client
             c = Client()
             c.notify(exception=e)
